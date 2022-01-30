cb885ecfc87fe2e82bbf5960e8e578d916cea5ca
Dhhonu <89118494+Dhhonu@users.noreply.github.com>
2022-01-30 14:19:02 +0530

    Update add_user.py

--- help/add_user.py
+++ help/add_user.py
@@ -1,6 +1,6 @@
-# (c) @AbirHasan2005
+# (c) @DKBOTZ
 
-from config import Config
+from bot import LOG_CHANNEL
 from help.access_db import db
 from pyrogram import Client
 from pyrogram.types import Message
@@ -9,7 +9,7 @@ from pyrogram.types import Message
 async def AddUserToDatabase(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
+        if LOG_CHANNEL is not None:
             await bot.send_message(
                 int(Config.LOG_CHANNEL),
                 f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
