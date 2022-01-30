e87a550f732e77f88448feebe93e7f1977c7fbfe
SriGuru05 <94064094+SriGuru05@users.noreply.github.com>
2022-01-30 22:34:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Dhhonu/my /my
+  git clone https://github.com/SriGuru05/my /my
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Dhhonu/my -b $BRANCH /my
+  git clone https://github.com/SriGuru05/my -b $BRANCH /my
 fi
 cd /forward
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... By @DKBOTZ"
+echo "Starting Bot.... By @MB_Links"
 python3 bot.py
